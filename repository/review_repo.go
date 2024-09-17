14f859296793399df390d7b742bcac1e9558d364
uqonstudio <sdesain25@gmail.com>
2024-09-17 09:55:41 +0700

    review_repo.go/Create - query for inser new review, success. end point POST http://localhost:2000/api/v1/review

--- repository/review_repo.go
+++ repository/review_repo.go
@@ -24,7 +24,19 @@ func NewReviewRepository(db *sql.DB) ReviewRepository {
 
 func (r *reviewRepository) Create(payload model.Review) (model.Review, error) {
 	var review model.Review
+	query := `
+        INSERT INTO t_review (user_id, cat_id, rating, comment, created_at, updated_at)
+        VALUES ($1, $2, $3, $4, NOW(), NOW())
+        RETURNING review_id, user_id, cat_id, rating, comment, created_at, updated_at
+    `
 
+	err := r.db.QueryRow(query, payload.ReviewID, payload.UserID, payload.CatID, payload.Rating, payload.Comment).Scan(
+		&review.ReviewID, &review.UserID, &review.CatID, &review.Rating, &review.Comment, &review.CreatedAt, &review.UpdatedAt,
+	)
+
+	if err != nil {
+		return model.Review{}, err
+	}
 	return review, nil
 }
 
